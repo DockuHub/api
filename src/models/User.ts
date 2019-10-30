377fdd72fb7cbac1ca3a7070ced8eddab02d4e65
andres-valdes <avald198@fiu.edu>
2019-10-29 23:43:58 -0400

    Removed TODO as it is not relevant to the file

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,7 +4,6 @@ import { BaseModel } from "@models/BaseModel";
 
 @Entity()
 export class User extends BaseModel {
-  // TODO Add missing columns (documents: Array<Document>, organizations: Array<Organization>, following: Array<User>, followers: Array<User>)
   @PrimaryGeneratedColumn("uuid")
   user_id!: string;
 
